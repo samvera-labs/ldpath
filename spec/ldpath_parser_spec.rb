ac161a61b995aa201d26f3d40d09a2d60823ddb7
Chris Beer <cabeer@stanford.edu>
2014-07-20 09:36:35 -0700

    Improve the parser tests

--- spec/ldpath_parser_spec.rb
+++ spec/ldpath_parser_spec.rb
@@ -4,61 +4,160 @@ require 'parslet/convenience'
 describe Ldpath::Parser do
   subject { Ldpath::Parser.new }
   context ".parse" do
-    it "should work" do
-      subject.parse ""
-      subject.parse "\t\t\n"
+    
+    describe "lines" do
+      it "should parse line-oriented data" do
+        subject.lines.parse " \n \n"
+      end
     end
     
-    it "should parse whitespace" do
-      subject.wsp.parse("\t\t\t")
+    describe "line" do
+      it "may be a line ending in a newline" do
+        subject.line.parse " \n"
+      end
+      
+      it "may be a line ending in EOF" do
+        subject.line.parse("/* abc */")
+      end
     end
     
-    it "should not parse comments" do
-      subject.line.parse "/* xyz */"
+    describe "newline" do
+      it 'may be a \n character' do
+        subject.newline.parse("\n")
+      end
+      
+      it 'may be a \n\r' do
+        subject.newline.parse("\n\r")
+      end
     end
     
-    it "should parse namespaces" do
-      subject.namespace.parse "@prefix a : <xyz>"
+    describe "eof" do
+      it "is the eof" do
+        subject.eof.parse ""
+      end
     end
     
-    it "should parse mappings" do
-      subject.parse("xyz = . ;\n")
+    describe "wsp" do
+      it "may be a space" do
+        subject.wsp.parse " "
+      end
+      
+      it "may be a tab" do
+        subject.wsp.parse "\t"
+      end
+      
+      it "may be a multiline comment" do
+        subject.wsp.parse "/* xyz */"
+      end
     end
     
-    it "should parse wildcards" do
-      subject.parse("xyz = * ;\n")
+    describe "expression" do
+      it "may be whitespace" do
+        subject.expression.parse " "
+      end
+      
+      it "may be a namespace declaration" do
+        subject.expression.parse "@prefix x : info:x ;"
+      end
+      
+      it "may be a graph" do
+        subject.expression.parse "@graph test:context, foo:ctx, test:bar ;"
+      end
+      
+      it "may be a mapping" do
+        subject.expression.parse "id = . ;"
+      end
     end
     
-    it "should parse reverse properties" do
-      subject.parse("xyz = ^info:a ;\n")
+    describe "uri" do
+      it "may be a bracketed uri" do
+        result = subject.uri.parse "<info:x>"
+        expect(result[:uri]).to eq "info:x"
+      end
+      
+      it "may be a namespace and local name" do
+        result = subject.uri.parse "info:x"
+        expect(result[:uri][:prefix]).to eq "info"
+        expect(result[:uri][:localName]).to eq "x"
+      end
     end
     
-    it "should parse uri mappings" do
-      subject.parse("xyz = <info:a> ;\n")
+    describe "identifier" do
+      it "must start with an alphanumeric character" do
+        subject.identifier.parse "a"
+        subject.identifier.parse "J"
+        subject.identifier.parse "4"
+        subject.identifier.parse "_"
+      end
+      
+      it "may have additional alphanumeric characters" do
+        subject.identifier.parse "aJ0_.-"
+      end
     end
     
-    it "should parse path mappings" do
-      subject.parse("xyz = info:a / info:b :: a:b;\n")
+    describe "strlit" do
+      it "is the content between \"" do
+        subject.strlit.parse '"abc"'
+      end
+      
+      it "should handle escaped characters" do
+        pending
+        subject.strlit.parse '"a\"b"'
+      end
     end
     
-    it "recursive_path_selector" do
-      subject.recursive_path_selector.parse("(foo:go)*")
-    end
-    it "strlit" do
-      subject.strlit.parse('" "')
-    end
-    it "function_selector" do
-      subject.selector.parse('fn:concat(foaf:givename," ",foaf:surname)')
-    end
-    
-    it "should parse the foaf example" do
-      subject.parse File.read(File.expand_path(File.join(__FILE__, "..", "fixtures", "foaf_example.program")))
+    describe "node" do
+      it "may be a uri" do
+        subject.node.parse "info:x"
+      end
+      
+      it "may be a literal" do
+        subject.node.parse '"a"'
+      end
     end
     
-    it "should parse the program.ldpath" do
-      subject.parse File.read(File.expand_path(File.join(__FILE__, "..", "fixtures", "program.ldpath")))
+    describe "selectors" do  
+      it "should parse mappings" do
+        subject.parse("xyz = . ;\n")
+      end
+      
+      it "should parse wildcards" do
+        subject.parse("xyz = * ;\n")
+      end
+      
+      it "should parse reverse properties" do
+        subject.parse("xyz = ^info:a ;\n")
+      end
+      
+      it "should parse uri mappings" do
+        subject.parse("xyz = <info:a> ;\n")
+      end
+      
+      it "should parse path mappings" do
+        subject.parse("xyz = info:a / info:b :: a:b;\n")
+      end
+      
+      it "recursive_path_selector" do
+        subject.recursive_path_selector.parse("(foo:go)*")
+      end
+      
+      it "function_selector" do
+        subject.selector.parse('fn:concat(foaf:givename," ",foaf:surname)')
+      end
     end
-  
-  
+    
+    describe "integration tests" do
+      it "should parse the foaf example" do
+        subject.parse File.read(File.expand_path(File.join(__FILE__, "..", "fixtures", "foaf_example.program")))
+      end
+      
+      it "should parse the program.ldpath" do
+        subject.parse File.read(File.expand_path(File.join(__FILE__, "..", "fixtures", "program.ldpath")))
+      end
+      
+      it "should parse the namespaces.ldpath" do
+        subject.parse File.read(File.expand_path(File.join(__FILE__, "..", "fixtures", "namespaces.ldpath")))
+      end
+    end  
   end
 end

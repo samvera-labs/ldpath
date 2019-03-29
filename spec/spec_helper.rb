550b110b01e5a936cb1f4b391e072430dda74ecd
E. Lynette Rayle <elr37@cornell.edu>
2019-03-29 14:20:52 -0400

    Add Coveralls for test coverage analysis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,24 @@ require 'ldpath'
 require 'rdf/reasoner'
 require 'webmock/rspec'
 
+require 'simplecov'
+require 'coveralls'
 require 'byebug' unless ENV['TRAVIS']
 
 RDF::Reasoner.apply(:rdfs)
 RDF::Reasoner.apply(:owl)
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start('rails') do
+  add_filter '/.internal_test_app'
+  add_filter '/lib/generators'
+  add_filter '/spec'
+  add_filter '/tasks'
+  add_filter '/lib/qa/version.rb'
+  add_filter '/lib/qa/engine.rb'
+end
+SimpleCov.command_name 'spec'
+Coveralls.wear!
 
 def webmock_fixture(fixture)
   File.new File.expand_path(File.join("../fixtures", fixture), __FILE__)

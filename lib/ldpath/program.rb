ef206da8a74ae8d3a32a0d5f11cde101ecdec726
Chris Beer <cabeer@stanford.edu>
2019-04-25 12:05:48 -0700

    Pull graph loading up to the program

--- lib/ldpath/program.rb
+++ lib/ldpath/program.rb
@@ -41,5 +41,15 @@ module Ldpath
 
       result.to_hash
     end
+
+    def load(uri)
+      Ldpath.logger.debug "Loading #{uri.inspect}"
+
+      reader_types = RDF::Format.reader_types.reject { |t| t.to_s =~ /html/ }.map do |t|
+        t.to_s =~ %r{text/(?:plain|html)} ? "#{t};q=0.5" : t
+      end
+
+      RDF::Graph.load(uri, headers: { 'Accept' => reader_types.join(", ") })
+    end
   end
 end

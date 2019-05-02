f9e9d6e6de4aed3b5592cf3ebb1ec394a5c6bccf
Chris Beer <cabeer@stanford.edu>
2019-05-02 11:58:52 -0700

    Add some retries to the LDF endpoint

--- lib/ldpath/loaders/linked_data_fragment.rb
+++ lib/ldpath/loaders/linked_data_fragment.rb
@@ -8,18 +8,26 @@ class Ldpath::Loaders::LinkedDataFragment
   end
 
   def load(uri)
-    Ldpath.logger.debug "Loading LDF data for #{uri.inspect}"
+    i = 0
+    begin
+      Ldpath.logger.debug "Loading LDF data for #{uri.inspect}"
 
-    graph = RDF::Graph.new
-    request_uri = RDF::URI("#{@endpoint}?subject=#{CGI::escape(uri)}")
+      graph = RDF::Graph.new
+      request_uri = RDF::URI("#{@endpoint}?subject=#{CGI::escape(uri)}")
 
-    while request_uri
-      Ldpath.logger.debug " -- querying #{request_uri}"
-      request_graph = RDF::Graph.load(request_uri)
-      graph.insert_statements(request_graph)
-      request_uri = request_graph.first_object([request_uri, NEXT_PAGE, nil])
+      while request_uri
+        Ldpath.logger.debug " -- querying #{request_uri}"
+        request_graph = RDF::Graph.load(request_uri)
+        graph.insert_statements(request_graph)
+        request_uri = request_graph.first_object([request_uri, NEXT_PAGE, nil])
+      end
+
+      graph
+    rescue => e
+      i += 1
+      retry if i < 3
+      Ldpath.logger.warn e
+      RDF::Graph.new
     end
-
-    graph
   end
 end
